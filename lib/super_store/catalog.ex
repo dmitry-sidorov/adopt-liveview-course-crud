3c73208f3aafcb3cf88866bf3719ada8d395f875
dmitry-sidorov <jimmaggot@mail.ru>
2024-10-10 18:23:57 +0300

    Add list products function

--- lib/super_store/catalog.ex
+++ lib/super_store/catalog.ex
@@ -2,6 +2,11 @@ defmodule SuperStore.Catalog do
   alias SuperStore.Repo
   alias SuperStore.Catalog.Product
 
+  def list_products do
+    Product
+    |> Repo.all()
+  end
+
   def create_product(attrs) do
     %Product{}
     |> Product.changeset(attrs)
